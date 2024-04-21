497996884f1dacb430a3b644ba17454e450d5252
morgan-b-kim <morgan-b-kim@gmail.com>
2024-04-21 12:17:35 +0900

    img

--- static/script.js
+++ static/script.js
@@ -14,27 +14,10 @@ document.addEventListener('mousemove', function(e) {
         img.style.transform = `translate(${imgX - img.width / 2}px, ${imgY - img.height / 2}px) scaleX(${lastDirection})`;
         lastX = mouseX;
     }
-    if (Date.now() - lastWaveTime > 500) {
-        createWave(mouseX, mouseY);
-        lastWaveTime = Date.now();
-    }
+
 });
 
-function createWave(x, y) {
-    console.log('Creating wave at:', x, y); // 파문 생성 로그 출력
-    const wave = document.createElement('div');
-    wave.className = 'wave';
-    wave.style.left = `${x - 150}px`; // 중앙 정렬을 위해 조정
-    wave.style.top = `${y - 150}px`; // 중앙 정렬을 위해 조정
-    wave.style.width = '300px'; // 원 크기 설정
-    wave.style.height = '300px'; // 원 크기 설정
-    waveContainer.appendChild(wave);
-    console.log('Wave added to container'); // DOM에 원 추가 로그 출력
-    setTimeout(() => {
-        wave.remove();
-        console.log('Wave removed'); // 원 제거 로그 출력
-    }, 500);
-}
+
 
 function followMouse() {
     requestAnimationFrame(followMouse);
